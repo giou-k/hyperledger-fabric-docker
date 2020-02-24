bb9ddc66f33925b72db9e586c8800eda3ba01027
giou-k <gioukonst@gmail.com>
2020-02-24 11:51:18 +0200

    Add CreateNetwork(), List() containers

--- cmd/main.go
+++ cmd/main.go
@@ -19,5 +19,27 @@ func main() {
 		os.Exit(1)
 	}
 
-	docker.BringUp()
+	// todo parse config from a config file.
+	c := docker.Config{}
+
+	cli, err := docker.NewClient()
+	if err != nil {
+		panic(err)
+	}
+	c = docker.Config{
+		Peer:     []string{
+			"peer0.org1.example.com",
+			"peer1.org1.example.com",
+		},
+		Orderer:  []string{
+			"orderer0.org1.example.com",
+		},
+		MyClient: cli,
+	}
+
+	err = c.CreateNetwork()
+	if err != nil {
+		log.Printf("CreateNetwork finished with error: %v", err)
+		os.Exit(1)
+	}
 }
--- docker/docker.go
+++ docker/docker.go
@@ -2,19 +2,52 @@ package docker
 
 import (
 	"context"
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"log"
 )
 
-func BringUp() {
+type CliInterface interface {
+	CreateNetwork() error
+	BringUp() error
+	List() error
+}
 
-	ctx := context.Background()
+type Config struct {
+	Peer    []string
+	Orderer []string
+
+	MyClient *client.Client
+}
+
+func (c *Config) CreateNetwork() error {
+	var err error
+
+	err = c.BringUp()
+	if err != nil {
+		return nil
+	}
+
+	err = c.List()
+	if err != nil {
+		return nil
+	}
+
+	return nil
+}
+
+func NewClient() (*client.Client, error) {
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		panic(err)
+		return cli, err
 	}
-	log.Println(cli)
+
+	return cli, err
+}
+
+func (c *Config) BringUp() error {
+	ctx := context.Background()
 
 	config := &container.Config{
 		Hostname:   "peer0.org1.example.com",
@@ -35,7 +68,7 @@ func BringUp() {
 			"CORE_PEER_GOSSIP_EXTERNALENDPOINT=peer0.org1.example.com:7051",
 			"CORE_PEER_LOCALMSPID=Org1MSP",
 		},
-		Cmd:   []string{"peer node start"},
+		Cmd:   []string{"peer", "node", "start"},
 		Image: "hyperledger/fabric-peer:1.4.4",
 		Volumes: map[string]struct{}{
 			"/var/run/:/host/var/run/": {},
@@ -47,10 +80,29 @@ func BringUp() {
 		NetworkDisabled: false,
 	}
 
-	resp, err := cli.ContainerCreate(ctx, config, nil, nil, "peer0.org1.example.com")
+	resp, err := c.MyClient.ContainerCreate(ctx, config, nil, nil,
+		"peer0.org1.example.com")
 	if err != nil {
-		panic(err)
+		return err
 	}
 	log.Println("containerCreate resp: ", resp)
 
+	if err := c.MyClient.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return err
+	}
+
+	return err
+}
+
+func (c *Config) List() error {
+	containers, err := c.MyClient.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		return err
+	}
+
+	for _, container := range containers {
+		log.Println(container.ID)
+	}
+
+	return nil
 }
